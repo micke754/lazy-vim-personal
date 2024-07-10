d29ad08e6a2f9d0ca1d7a4181d11ce1b5cce46be
Kevin Michael <micke51p@UO115076.registry.otago.ac.nz>
2024-07-10 14:25:30 +1200

    More changes to be inline with Kickstart

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,14 @@ vim.keymap.set("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left w
 vim.keymap.set("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- NeoTree Direction Change
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal_force_cwd right<CR>", {})
+
+vim.keymap.set("n", "<leader>E", ":Neotree filesystem reveal dir=~ right<CR>", {})
+
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
