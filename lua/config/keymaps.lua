6654c1ac046f431357d02f0ee179c3299afaf467
Kevin Michael <micke51p@UO115076.registry.otago.ac.nz>
2024-07-10 13:39:32 +1200

    First initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>sf", LazyVim.pick("auto"), { desc = "[S]earch [F]iles (Root Dir)" })
+
+-- Easier Commandline
+vim.keymap.set("n", ";", ":", { desc = "Quicker Commandline" })
+
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set("n", "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
