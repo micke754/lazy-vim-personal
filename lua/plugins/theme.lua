ef2f390dd2795764109d39c40696f0995a738291
Kevin Michael <micke51p@UO115076.registry.otago.ac.nz>
2024-07-10 15:32:45 +1200

    Adding kanagawa to themes

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,15 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- add kanagawa
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
