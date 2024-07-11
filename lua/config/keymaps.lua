3733de1bf546a62b76487909628b3e59aed921fd
Kevin Michael <kevin.vncnt@gmail.com>
2024-07-11 15:10:16 +1200

    Created a .lua file for disabling plugins + some iterative changes.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>sf", LazyVim.pick("auto"), { desc = "[S]earch [F]iles (Root Dir)" })
+-- Telescope find files in current directory
+-- vim.keymap.set("n", "<leader>sf", LazyVim.pick("auto"), { desc = "[S]earch [F]iles (Root Dir)" })
 
 -- Easier Commandline
 vim.keymap.set("n", ";", ":", { desc = "Quicker Commandline" })
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = true,
+  },
+  {
+    "lukus-reineke/headlines.nvim",
+    enabled = false,
+  },
+}
